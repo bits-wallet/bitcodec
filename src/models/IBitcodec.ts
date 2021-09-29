c83a12ac859010c21a7808671532bb3b30739c74
Penguen Umut <uuisik@gmail.com>
2021-09-29 14:31:06 +0300

    IBitcodec, NumberTypes model

---
+++ src/models/IBitcodec.ts
@@ -0,0 +1,7 @@
+export interface IBitcodec<T> {
+  encodeBytes: number;
+  decodeBytes: number;
+  encodingLength: (t?: T) => number;
+  encode: (value: T, buffer: Buffer, offset?: number) => Buffer;
+  decode: (buffer: Buffer, offset?: number, end?: number) => T;
+}
