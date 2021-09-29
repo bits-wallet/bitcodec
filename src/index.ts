ca848057e63b5be91e47a8946ee275a9aac4b9aa
Penguen Umut <uuisik@gmail.com>
2021-09-29 14:41:30 +0300

    Number types codecs export

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { NumberTypes } from "./models/NumberTypes";
+import { CNumber } from "./lib/CNumber";
+
+export default {
+  Byte: new CNumber(NumberTypes.UInt8, 1),
+  Int8: new CNumber(NumberTypes.Int8, 1),
+  UInt8: new CNumber(NumberTypes.UInt8, 1),
+  Int16BE: new CNumber(NumberTypes.Int16BE, 2),
+  Int16LE: new CNumber(NumberTypes.Int16LE, 2),
+  UInt16BE: new CNumber(NumberTypes.UInt16BE, 2),
+  UInt16LE: new CNumber(NumberTypes.UInt16LE, 2),
+  Int32BE: new CNumber(NumberTypes.Int32BE, 4),
+  Int32LE: new CNumber(NumberTypes.Int32LE, 4),
+  UInt32BE: new CNumber(NumberTypes.UInt32BE, 4),
+  UInt32LE: new CNumber(NumberTypes.UInt32LE, 4),
+  Int64BE: new CNumber(NumberTypes.Int64BE, 8),
+  Int64LE: new CNumber(NumberTypes.Int64LE, 8),
+  UInt64BE: new CNumber(NumberTypes.UInt64BE, 8),
+  UInt64LE: new CNumber(NumberTypes.UInt64LE, 8),
+  FloatBE: new CNumber(NumberTypes.FloatBE, 4),
+  FloatLE: new CNumber(NumberTypes.FloatLE, 4),
+  DoubleBE: new CNumber(NumberTypes.DoubleBE, 8),
+  DoubleLE: new CNumber(NumberTypes.DoubleLE, 8),
+};
