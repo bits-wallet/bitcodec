065565b846cc5b3af98d0b45f1d6d0f2b6b7aeb9
Penguen Umut <uuisik@gmail.com>
2021-10-01 00:00:20 +0300

    ENcodingType model

--- src/lib/CVarString.ts
+++ src/lib/CVarString.ts
@@ -1,20 +1,19 @@
+import { EncodingType } from "../models/EncodingType";
 import { IBitcodec } from "../models/IBitcodec";
 import { CVarBuffer } from "./CVarBuffer";
 
-type Encodings = "ascii" | "utf8" | "utf16le" | "ucs2" | "base64" | "hex";
-
 export class CVarString implements IBitcodec<string> {
   private anyCodec: IBitcodec<any>;
-  private encoding: Encodings;
+  private encodingType: EncodingType;
   private varBufferCodec: CVarBuffer;
   encodingLength: (value?: string) => number;
 
   encodeBytes: number;
   decodeBytes: number;
 
-  constructor(anyCodec: IBitcodec<any>, encoding: Encodings = "utf8") {
+  constructor(anyCodec: IBitcodec<any>, encodingType: EncodingType = "utf8") {
     this.anyCodec = anyCodec;
-    this.encoding = encoding;
+    this.encodingType = encodingType;
     this.varBufferCodec = new CVarBuffer(anyCodec);
 
     this.encodeBytes = 0;
@@ -22,13 +21,13 @@ export class CVarString implements IBitcodec<string> {
 
     this.encodingLength = (value?: string) => {
       if (value === undefined) throw new TypeError("value must be a string");
-      const valueLength = Buffer.byteLength(value, this.encoding);
+      const valueLength = Buffer.byteLength(value, this.encodingType);
       return this.anyCodec.encodingLength(value.length) + valueLength;
     };
   }
 
   encode = (value: string, buffer?: Buffer, offset = 0): Buffer => {
-    const valueLength = Buffer.byteLength(value, this.encoding);
+    const valueLength = Buffer.byteLength(value, this.encodingType);
     const bytes = this.anyCodec.encodingLength(value.length) + valueLength;
 
     if (!buffer) buffer = Buffer.allocUnsafe(bytes);
@@ -36,14 +35,14 @@ export class CVarString implements IBitcodec<string> {
 
     this.anyCodec.encode(valueLength, buffer, offset);
     offset += this.anyCodec.encodeBytes;
-    buffer.write(value, offset, valueLength, this.encoding);
+    buffer.write(value, offset, valueLength, this.encodingType);
 
     this.encodeBytes = bytes;
     return buffer;
   };
 
   decode = (buffer: Buffer, offset = 0, end?: number): string => {
-    const str = this.varBufferCodec.decode(buffer, offset, end).toString(this.encoding);
+    const str = this.varBufferCodec.decode(buffer, offset, end).toString(this.encodingType);
     this.decodeBytes = this.varBufferCodec.decodeBytes;
     return str;
   };
