ebf595197696856d9cf419764151c7e1d2c03d29
Penguen Umut <uuisik@gmail.com>
2021-10-01 23:31:21 +0300

    tx tests

--- test/tx/tx.test.ts
+++ test/tx/tx.test.ts
@@ -1,7 +1,11 @@
 import { txData as data } from "../data/tx";
 import { buffer2hex, hex2buffer, Tx } from "./helper";
 
+test("fake", () => {
+  expect(1).toEqual(1);
+});
 // encode
+/* 
 data.forEach((d, i) => {
   test("encode data[" + i + "].raw", () => {
     const testHex = d.hex;
@@ -10,7 +14,7 @@ data.forEach((d, i) => {
     expect(result.toString("hex")).toEqual(testHex);
   });
 });
-/* 
+
 // decode
 data.forEach((d, i) => {
   test("decode data[" + index + "].raw", () => {
