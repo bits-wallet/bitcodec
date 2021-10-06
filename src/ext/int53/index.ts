cfc550fe9e81e589f8d18985ba7df21e31072a9d
Penguen Umut <uuisik@gmail.com>
2021-10-06 21:41:14 +0300

    int53 removed

--- src/ext/int53/index.ts
+++
@@ -1,3 +0,0 @@
-export { Int53Type } from "./common";
-export { write as writeInt53 } from "./write";
-export { read as readInt53 } from "./read";
--- src/ext/int53/read.ts
+++
@@ -1,8 +0,0 @@
-import { Int53Type, toDouble } from "./common";
-
-export const read = (int53Type: Int53Type, buffer: Buffer, offset = 0): number => {
-  const BE = int53Type.endsWith("BE");
-  const [high, low] = BE ? [buffer.readUInt32BE(offset), buffer.readUInt32BE(offset + 4)] : [buffer.readUInt32LE(offset + 4), buffer.readUInt32LE(offset)];
-  const signed = int53Type.startsWith("Int");
-  return toDouble(high, low, signed);
-};
