a5ebb6855a937083eabe2fa5fabf114001aa3b8f
Penguen Umut <uuisik@gmail.com>
2021-10-06 21:41:02 +0300

    test data

---
+++ test/data/tx_new.ts
@@ -0,0 +1,58 @@
+export const txNewDatas = [
+  {
+    hex: "0100000002d8c8df6a6fdd2addaf589a83d860f18b44872d13ee6ec3526b2b470d42a96d4d000000008b483045022100b31557e47191936cb14e013fb421b1860b5e4fd5d2bc5ec1938f4ffb1651dc8902202661c2920771fd29dd91cd4100cefb971269836da4914d970d333861819265ba014104c54f8ea9507f31a05ae325616e3024bd9878cb0a5dff780444002d731577be4e2e69c663ff2da922902a4454841aa1754c1b6292ad7d317150308d8cce0ad7abffffffff2ab3fa4f68a512266134085d3260b94d3b6cfd351450cff021c045a69ba120b2000000008b4830450220230110bc99ef311f1f8bda9d0d968bfe5dfa4af171adbef9ef71678d658823bf022100f956d4fcfa0995a578d84e7e913f9bb1cf5b5be1440bcede07bce9cd5b38115d014104c6ec27cffce0823c3fecb162dbd576c88dd7cda0b7b32b0961188a392b488c94ca174d833ee6a9b71c0996620ae71e799fc7c77901db147fa7d97732e49c8226ffffffff02c0175302000000001976a914a3d89c53bb956f08917b44d113c6b2bcbe0c29b788acc01c3d09000000001976a91408338e1d5e26db3fce21b011795b1c3c8a5a5d0788ac00000000",
+    raw: {
+      txid: "9021b49d445c719106c95d561b9c3fac7bcb3650db67684a9226cd7fa1e1c1a0",
+      hash: "9021b49d445c719106c95d561b9c3fac7bcb3650db67684a9226cd7fa1e1c1a0",
+      version: 1,
+      size: 438,
+      vsize: 438,
+      weight: 1752,
+      locktime: 0,
+      vin: [
+        {
+          txid: "4d6da9420d472b6b52c36eee132d87448bf160d8839a58afdd2add6f6adfc8d8",
+          vout: 0,
+          scriptSig: {
+            asm: "3045022100b31557e47191936cb14e013fb421b1860b5e4fd5d2bc5ec1938f4ffb1651dc8902202661c2920771fd29dd91cd4100cefb971269836da4914d970d333861819265ba[ALL] 04c54f8ea9507f31a05ae325616e3024bd9878cb0a5dff780444002d731577be4e2e69c663ff2da922902a4454841aa1754c1b6292ad7d317150308d8cce0ad7ab",
+            hex: "483045022100b31557e47191936cb14e013fb421b1860b5e4fd5d2bc5ec1938f4ffb1651dc8902202661c2920771fd29dd91cd4100cefb971269836da4914d970d333861819265ba014104c54f8ea9507f31a05ae325616e3024bd9878cb0a5dff780444002d731577be4e2e69c663ff2da922902a4454841aa1754c1b6292ad7d317150308d8cce0ad7ab",
+          },
+          sequence: 4294967295,
+        },
+        {
+          txid: "b220a19ba645c021f0cf501435fd6c3b4db960325d0834612612a5684ffab32a",
+          vout: 0,
+          scriptSig: {
+            asm: "30450220230110bc99ef311f1f8bda9d0d968bfe5dfa4af171adbef9ef71678d658823bf022100f956d4fcfa0995a578d84e7e913f9bb1cf5b5be1440bcede07bce9cd5b38115d[ALL] 04c6ec27cffce0823c3fecb162dbd576c88dd7cda0b7b32b0961188a392b488c94ca174d833ee6a9b71c0996620ae71e799fc7c77901db147fa7d97732e49c8226",
+            hex: "4830450220230110bc99ef311f1f8bda9d0d968bfe5dfa4af171adbef9ef71678d658823bf022100f956d4fcfa0995a578d84e7e913f9bb1cf5b5be1440bcede07bce9cd5b38115d014104c6ec27cffce0823c3fecb162dbd576c88dd7cda0b7b32b0961188a392b488c94ca174d833ee6a9b71c0996620ae71e799fc7c77901db147fa7d97732e49c8226",
+          },
+          sequence: 4294967295,
+        },
+      ],
+      vout: [
+        {
+          value: 0.39,
+          n: 0,
+          scriptPubKey: {
+            asm: "OP_DUP OP_HASH160 a3d89c53bb956f08917b44d113c6b2bcbe0c29b7 OP_EQUALVERIFY OP_CHECKSIG",
+            hex: "76a914a3d89c53bb956f08917b44d113c6b2bcbe0c29b788ac",
+            reqSigs: 1,
+            type: "pubkeyhash",
+            addresses: ["mvTHvhE8wzQyNunNVJnZcQgsZzpAYSDQ3p"],
+          },
+        },
+        {
+          value: 1.55,
+          n: 1,
+          scriptPubKey: {
+            asm: "OP_DUP OP_HASH160 08338e1d5e26db3fce21b011795b1c3c8a5a5d07 OP_EQUALVERIFY OP_CHECKSIG",
+            hex: "76a91408338e1d5e26db3fce21b011795b1c3c8a5a5d0788ac",
+            reqSigs: 1,
+            type: "pubkeyhash",
+            addresses: ["mgGKThgk2gkrwmhqubaoWthao5L8L3BgX1"],
+          },
+        },
+      ],
+    },
+  },
+];
