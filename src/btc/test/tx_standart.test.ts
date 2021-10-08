54597b1eff2fad8bae5016a28dcf9e21e0afe496
Penguen Umut <uuisik@gmail.com>
2021-10-08 20:24:07 +0300

    tx standart encode decode test ok

---
+++ src/btc/test/tx_standart.test.ts
@@ -0,0 +1,23 @@
+import { buffer2hex, hex2buffer } from "../../btc/helper";
+import { Tx } from "../../btc/someCodecs";
+import { datas } from "./data/tx_standart";
+
+datas.forEach((data, index) => {
+  test("tx_standart decode index:" + index, () => {
+    const txHex = data.hex;
+    const txRaw = data.raw;
+
+    const resultBuffer = Tx.decode(hex2buffer(txHex));
+    const result = buffer2hex(resultBuffer);
+    expect(result).toEqual(txRaw);
+  });
+
+  test("tx_standart encode index:" + index, () => {
+    const txHex = data.hex;
+    const txRaw = data.raw;
+
+    const resultBuffer = Tx.encode(hex2buffer(txRaw));
+    const result = buffer2hex(resultBuffer);
+    expect(result).toEqual(txHex);
+  });
+});
