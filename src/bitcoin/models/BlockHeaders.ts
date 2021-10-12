1ca5e700dc578023b93c679c5545a1b74429dd7f
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-10-12 21:55:14 +0300

    Used same names in doc as camelCase, Added doc url in comment on models, header fixed to as headers

--- src/bitcoin/models/BlockHeaders.ts
+++ src/bitcoin/models/BlockHeaders.ts
@@ -1,16 +1,20 @@
 import { MessageHeader } from "./MessageHeader";
 
+// https://developer.bitcoin.org/reference/block_chain.html#block-headers
+
 export type BlockHeader = {
   version: number;
-  prevHash: string;
-  merkleRoot: string;
-  timestamp: number;
-  bits: number;
+  previousBlockHeaderHash: string;
+  merkleRootHash: string;
+  time: number;
+  nBits: number;
   nonce: number;
 };
 
+// https://developer.bitcoin.org/reference/p2p_networking.html#getheaders
+
 export type BlockHeadersPayload = {
-  header: BlockHeader;
+  headers: BlockHeader;
   count: number;
 };
 
