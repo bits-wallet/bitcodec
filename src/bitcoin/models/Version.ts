7c5fccbc5f99bddbf018468bd10774752df505f1
Penguen Umut <uuisik@gmail.com>
2021-10-12 12:07:37 +0300

    Version extends MessageHeader with payload

--- src/bitcoin/models/Version.ts
+++ src/bitcoin/models/Version.ts
@@ -1,16 +1,21 @@
+import { MessageHeader } from "./MessageHeader";
+
 // https://developer.bitcoin.org/reference/p2p_networking.html#version
-export type Version = {
+type Addr = {
+  services: Buffer;
+  iPAddress: string;
+  port: number;
+};
+export type VersionPayload = {
   version: number;
   services: Buffer;
   timestamp: number;
-  addrRecvServices: Buffer;
-  addrRecvIPAddress: string;
-  addrRecvPort: number;
-  addrTransServices: Buffer;
-  addrTransIPAddress: string;
-  addrTransPort: number;
-  nonce: number;
+  addrRecv: Addr;
+  addrTrans: Addr;
+  nonce: Buffer;
   userAgent: string;
   startHeight: number;
   relay: boolean;
 };
+
+export interface Version extends MessageHeader<VersionPayload> {}
