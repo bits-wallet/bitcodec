259d53fe57c684b0ed702cedd295d0fa0994fe78
Penguen Umut <uuisik@gmail.com>
2021-10-14 00:28:01 +0300

    MessageStructure MAGIC enum

--- src/bitcoin/models/p2p/MessageStructure.ts
+++ src/bitcoin/models/p2p/MessageStructure.ts
@@ -1,6 +1,8 @@
+import { MAGIC } from "./MAGIC";
+
 // https://en.bitcoin.it/wiki/Protocol_documentation#Message_structure
 export interface MessageStructure<T> {
-  magic: number;
+  magic: MAGIC;
   command: string;
   length: number;
   checksum: string;
