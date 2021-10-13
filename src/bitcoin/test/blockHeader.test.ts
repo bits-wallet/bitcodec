09a36f55b8550916e0f4cc9ed35d259f2b02954b
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-10-13 15:23:15 +0300

    BlockHeader test

--- src/bitcoin/test/blockHeader.test.ts
+++ src/bitcoin/test/blockHeader.test.ts
@@ -1,9 +1,29 @@
 import * as bitcoin from "../../bitcoin";
-import { datas } from "./data/message_header";
+import { datas } from "./data/block_header";
 
-test("blockheader decode:", () => {
-  const headerAllData = bitcoin.HeaderCodec.decode(datas[0].hex);
-  const result = bitcoin.BlockHeaderCodec.decode(headerAllData.payload);
+datas.forEach((data, index) => {
+  test("block_header decode index:" + index, () => {
+    const headerAllData = bitcoin.HeaderCodec.decode(data.hex);
+    const result = bitcoin.BlockHeaderCodec.decode(headerAllData.payload);
 
-  console.log(result);
+    expect(result[0].headers.version).toEqual(data.raw.payload[0].headers.version);
+    expect(result[0].headers.previousBlockHeaderHash).toEqual(data.raw.payload[0].headers.previousBlockHeaderHash);
+    expect(result[0].headers.merkleRootHash).toEqual(data.raw.payload[0].headers.merkleRootHash);
+  });
+
+  test("block_header encode index:" + index, () => {
+    const payloadDataHex = bitcoin.BlockHeaderCodec.encode(data.raw.payload);
+
+    const headerData = {
+      startString: data.raw.startString,
+      commandName: data.raw.commandName,
+      payloadSize: data.raw.payloadSize,
+      checksum: data.raw.checksum,
+      payload: payloadDataHex,
+    };
+
+    const result = bitcoin.HeaderCodec.encode(headerData);
+
+    expect(result).toEqual(data.hex);
+  });
 });
