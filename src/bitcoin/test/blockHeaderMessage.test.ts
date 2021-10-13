122e7eeb5882ca3354d38ba19401ca50b75816ca
afarukcali <afarukcali@gmail.com>
2021-10-14 02:44:23 +0300

    block header message data hex changed with true hex text end block header encode text added

--- src/bitcoin/test/blockHeaderMessage.test.ts
+++ src/bitcoin/test/blockHeaderMessage.test.ts
@@ -1,13 +1,14 @@
 import * as bitcoin from "../../bitcoin";
 import { buffer2hex, hex2buffer } from "../helper";
 import { BlockHeader } from "../models/BlockHeader";
-import { datas } from "./data/block_header_message";
+import { MessageStructure } from "../models/p2p/MessageStructure";
+import { data } from "./data/block_header_message";
 
 test("block_header decode:", () => {
-  const headerAllData = bitcoin.MessageStructureCodec.decode(hex2buffer(datas[0].hex));
+  const headerAllData = bitcoin.MessageStructureCodec.decode(hex2buffer(data[0].hex));
   const result = bitcoin.BlockHeadersCodec.decode(hex2buffer(headerAllData.payload));
 
-  const testData: BlockHeader[] = datas[0].raw.payload;
+  const testData: BlockHeader[] = data[0].raw.payload;
 
   result.forEach((res: any, index: number) => {
     expect(res.bits).toEqual(testData[index].bits);
@@ -18,4 +19,23 @@ test("block_header decode:", () => {
     expect(buffer2hex(res.prevBlock)).toEqual(testData[index].prevBlock);
     expect(buffer2hex(res.merkleRoot)).toEqual(testData[index].merkleRoot);
   });
+});
+
+test("block_header encode:", () => {
+  const testData = data[0];
+  const blockHeaderPayloadBuffer = bitcoin.BlockHeadersCodec.encode(hex2buffer(testData.raw.payload));
+  const blockHeaderPayloadHex: string = buffer2hex(blockHeaderPayloadBuffer);
+
+  console.log(blockHeaderPayloadHex);
+
+  const blcokHeaderMessageStructure: MessageStructure<string> = {
+    magic: testData.raw.magic,
+    command: testData.raw.command,
+    length: testData.raw.length,
+    checksum: testData.raw.checksum,
+    payload: blockHeaderPayloadHex,
+  };
+
+  const blockHeaderMessageHex = bitcoin.MessageStructureCodec.encode(hex2buffer(blcokHeaderMessageStructure));
+  expect(blockHeaderMessageHex).toEqual(testData.hex);
 });
