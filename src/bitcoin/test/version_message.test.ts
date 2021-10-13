c09a8e6927d5180e8861769ed61c8127f18f81ff
Penguen Umut <uuisik@gmail.com>
2021-10-13 23:18:56 +0300

    versionMessage test types fix

--- src/bitcoin/test/version_message.test.ts
+++ src/bitcoin/test/version_message.test.ts
@@ -1,19 +1,21 @@
 import * as bitcoin from "../../bitcoin";
 import { buffer2hex, hex2buffer } from "../helper";
+import { MessageHeader } from "../models/p2p/MessageHeader";
 import { Version, VersionMessage } from "../models/p2p/Version";
 import { datas } from "./data/version_message";
 
 datas.forEach((data, index) => {
   test("version message decode index:" + index, () => {
-    const headerAllData = bitcoin.HeaderCodec.decode(data.hex);
-    const versionPayloadBuffer = bitcoin.VersionCodec.decode(hex2buffer(headerAllData.payload));
-    expect(buffer2hex(versionPayloadBuffer)).toEqual(buffer2hex(data.raw.payload));
+    const versionMessageBuffer = bitcoin.HeaderCodec.decode(data.hex);
+    const version = hex2buffer(versionMessageBuffer.payload);
+    const versionBuffer = bitcoin.VersionCodec.decode(version);
+    expect(buffer2hex(versionBuffer)).toEqual(buffer2hex(data.raw.payload));
   });
 
   test("version message encode index:" + index, () => {
     const versionBuffer = bitcoin.VersionCodec.encode(hex2buffer(data.raw.payload));
-    const version: Version = buffer2hex(versionBuffer);
-    const headerAllData: VersionMessage = {
+    const version: string = buffer2hex(versionBuffer);
+    const headerAllData: MessageHeader<string> = {
       startString: data.raw.startString,
       commandName: data.raw.commandName,
       payloadSize: data.raw.payloadSize,
@@ -21,7 +23,7 @@ datas.forEach((data, index) => {
       payload: version,
     };
 
-    const versionDataBuffer = bitcoin.HeaderCodec.encode(hex2buffer(headerAllData));
-    expect(buffer2hex(versionDataBuffer)).toEqual(data.hex);
+    const versionMessageHex = bitcoin.HeaderCodec.encode(hex2buffer(headerAllData));
+    expect(buffer2hex(versionMessageHex)).toEqual(data.hex);
   });
 });
