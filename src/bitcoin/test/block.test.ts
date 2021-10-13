2713348ee6633d27bc70c6e2bce4fed8364a8c5f
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-10-13 17:25:39 +0300

    block tests

---
+++ src/bitcoin/test/block.test.ts
@@ -0,0 +1,42 @@
+import * as bitcoin from "../../bitcoin";
+import { BlockPayload } from "../models/Block";
+import { datas } from "./data/block";
+
+datas.forEach((data, index) => {
+  test("block decode index:" + index, () => {
+    const decodedData = bitcoin.HeaderCodec.decode(data.hex);
+    const decodedBlockPayload: BlockPayload = bitcoin.BlockCodec.decode(decodedData.payload);
+
+    // blockHeader
+    expect(decodedBlockPayload.blockHeader.merkleRootHash).toEqual(data.raw.payload.blockHeader.merkleRootHash);
+    expect(decodedBlockPayload.blockHeader.nBits).toEqual(data.raw.payload.blockHeader.nBits);
+    expect(decodedBlockPayload.blockHeader.nonce).toEqual(data.raw.payload.blockHeader.nonce);
+    expect(decodedBlockPayload.blockHeader.previousBlockHeaderHash).toEqual(data.raw.payload.blockHeader.previousBlockHeaderHash);
+    expect(decodedBlockPayload.blockHeader.time).toEqual(data.raw.payload.blockHeader.time);
+    expect(decodedBlockPayload.blockHeader.version).toEqual(data.raw.payload.blockHeader.version);
+
+    // txns
+    expect(decodedBlockPayload.txns[0].lockTime).toEqual(data.raw.payload.txns[0].lockTime);
+    expect(decodedBlockPayload.txns[0].txIn[0].previousOutput).toEqual(data.raw.payload.txns[0].txIn[0].previousOutput);
+    expect(decodedBlockPayload.txns[0].txIn[0].sequence).toEqual(data.raw.payload.txns[0].txIn[0].sequence);
+    expect(decodedBlockPayload.txns[0].txIn[0].signatureScript).toEqual(data.raw.payload.txns[0].txIn[0].signatureScript);
+    expect(decodedBlockPayload.txns[0].txOut[0].pkScript).toEqual(data.raw.payload.txns[0].txOut[0].pkScript);
+    expect(decodedBlockPayload.txns[0].txOut[0].value).toEqual(data.raw.payload.txns[0].txOut[0].value);
+  });
+
+  test("block encode index:" + index, () => {
+    const blockPayloadDataHex = bitcoin.BlockCodec.encode(data.raw.payload);
+
+    const blockData = {
+      startString: data.raw.startString,
+      commandName: data.raw.commandName,
+      payloadSize: data.raw.payloadSize,
+      checksum: data.raw.checksum,
+      payload: blockPayloadDataHex,
+    };
+
+    const result = bitcoin.HeaderCodec.encode(blockData);
+
+    expect(result).toEqual(data.hex);
+  });
+});
