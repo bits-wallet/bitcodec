a1184ae64abdf3e16683d8caa9fbf1b901326775
Penguen Umut <uuisik@gmail.com>
2021-10-15 02:53:43 +0300

    umd tests

--- dist/bitcoin.d.ts
+++ dist/bitcoin.d.ts
@@ -3,4 +3,5 @@ import { MessageStructureCodec } from "./bitcoin/codec/p2p/MessageStructureCodec
 import { VersionCodec } from "./bitcoin/codec/p2p/VersionCodec";
 import { TxCodec } from "./bitcoin/codec/TxCodec";
 import { InventoriesCodec } from "./bitcoin/codec/p2p/InventoriesCodec";
-export { MessageStructureCodec, VersionCodec, InventoriesCodec, TxCodec, BlockHeaderCodec, BlockHeaderArrayCodec };
+import * as helper from "./bitcoin/helper";
+export { MessageStructureCodec, VersionCodec, InventoriesCodec, TxCodec, BlockHeaderCodec, BlockHeaderArrayCodec, helper };
--- src/bitcoin.ts
+++ src/bitcoin.ts
@@ -3,5 +3,6 @@ import { MessageStructureCodec } from "./bitcoin/codec/p2p/MessageStructureCodec
 import { VersionCodec } from "./bitcoin/codec/p2p/VersionCodec";
 import { TxCodec } from "./bitcoin/codec/TxCodec";
 import { InventoriesCodec } from "./bitcoin/codec/p2p/InventoriesCodec";
+import * as helper from "./bitcoin/helper";
 
-export { MessageStructureCodec, VersionCodec, InventoriesCodec, TxCodec, BlockHeaderCodec, BlockHeaderArrayCodec };
+export { MessageStructureCodec, VersionCodec, InventoriesCodec, TxCodec, BlockHeaderCodec, BlockHeaderArrayCodec, helper };
