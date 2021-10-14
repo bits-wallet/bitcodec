77a7315064f28269f298d96abf96a492e4688a58
Penguen Umut <uuisik@gmail.com>
2021-10-15 01:34:21 +0300

    export bitcoin lib ref fix

--- src/bitcoin.ts
+++ src/bitcoin.ts
@@ -1,8 +1,7 @@
-// import { MessageCommandCodec } from "./bitcoin/MessageCommandCodec";
-import { MessageStructureCodec } from "./bitcoin/MessageStructureCodec";
-import { VersionCodec } from "./bitcoin/VersionCodec";
-import { TxCodec } from "./bitcoin/TxCodec";
-import { InventoriesCodec } from "./bitcoin/InventoriesCodec";
-import { BlockHeaderCodec, BlockHeaderArrayCodec } from "./bitcoin/BlockHeadersCodec";
+import { BlockHeaderCodec, BlockHeaderArrayCodec } from "./bitcoin/codec/bitcodecObjects";
+import { MessageStructureCodec } from "./bitcoin/codec/p2p/MessageStructureCodec";
+import { VersionCodec } from "./bitcoin/codec/p2p/VersionCodec";
+import { TxCodec } from "./bitcoin/codec/TxCodec";
+import { InventoriesCodec } from "./bitcoin/codec/p2p/InventoriesCodec";
 
 export { MessageStructureCodec, VersionCodec, InventoriesCodec, TxCodec, BlockHeaderCodec, BlockHeaderArrayCodec };
