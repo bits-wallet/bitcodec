2def82840a6d389da5cf860ec2c313c26cf2768a
Penguen Umut <uuisik@gmail.com>
2021-10-17 23:00:23 +0300

    model module

---
+++ src/bitcoin/models/index.ts
@@ -0,0 +1,4 @@
+export * from "./p2p";
+export * from "./BlockHeader";
+export * from "./Block";
+export * from "./Tx";
--- src/bitcoin/models/p2p/MAGIC.ts
+++ src/bitcoin/models/p2p/MAGIC.ts
@@ -1,8 +1,8 @@
 // https://en.bitcoin.it/wiki/Protocol_documentation#Message_structure
 export enum MAGIC {
-  Mainnet = 3652501241, // 0xD9B4BEF9
-  Regtest = 3669344250, // 0xDAB5BFFA
-  Testnet3 = 118034699, // 0x0709110B
-  Signet = 1087308554, // 0x40CF030A (default)
-  Namecoin = 4273258233, // 0xFEB4BEF9
+  Mainnet = 0xd9b4bef9, // 3652501241
+  Regtest = 0xdab5bffa, // 3669344250
+  Testnet3 = 0x0709110b, // 118034699
+  Signet = 0x40cf030a, // 1087308554 (default)
+  Namecoin = 0xfeb4bef9, // 4273258233
 }
