7b3e244f5c2f159838a55d5731640e6dc1c399e9
Penguen Umut <uuisik@gmail.com>
2021-10-20 18:24:43 +0300

    CVarBuffer errors check, constructor type changed

--- src/lib/CVarBuffer.ts
+++ src/lib/CVarBuffer.ts
@@ -1,27 +1,32 @@
+import { checkBufferLengthForDecode, checkBufferLengthForEncode, checkDefined } from "../errors";
 import { IBitcodec } from "../models/IBitcodec";
+import { CNumber } from "./CNumber";
+import { CVarUIntBitcoin } from "./CVarUIntBitcoin";
 
 export class CVarBuffer implements IBitcodec<any> {
+  private codecName = "VarBuffer";
   private anyCodec: IBitcodec<any>;
   encodingLength: (buffer?: Buffer) => number;
 
   encodeBytes: number;
   decodeBytes: number;
 
-  constructor(anyCodec: IBitcodec<any>) {
+  constructor(anyCodec: IBitcodec<CNumber | CVarUIntBitcoin>) {
     this.anyCodec = anyCodec;
     this.encodeBytes = 0;
     this.decodeBytes = 0;
 
     this.encodingLength = (buffer?: Buffer) => {
-      if (buffer === undefined) throw new TypeError("value must be a Buffer instance"); // for CArray encode iter
-      return this.anyCodec.encodingLength(buffer.length) + buffer.length;
+      checkDefined(this.codecName, buffer, "buffer"); // for CArray encode iter
+      if (buffer !== undefined) return this.anyCodec.encodingLength(buffer.length) + buffer.length;
+      return 0; // never
     };
   }
 
   encode = (value: Buffer, buffer?: Buffer, offset = 0): Buffer => {
     const bytes = this.encodingLength(value);
     if (!buffer) buffer = Buffer.allocUnsafe(bytes);
-    else if (buffer.length - offset < bytes) throw new RangeError("destination buffer is too small");
+    else checkBufferLengthForEncode(this.codecName, buffer, offset, bytes);
 
     this.anyCodec.encode(value.length, buffer, offset);
     offset += this.anyCodec.encodeBytes;
@@ -35,11 +40,9 @@ export class CVarBuffer implements IBitcodec<any> {
   decode = (buffer: Buffer, offset = 0, end?: number): Buffer => {
     if (end === undefined) end = buffer.length;
     const start = offset;
-
     const length = this.anyCodec.decode(buffer, offset, end);
     offset += this.anyCodec.decodeBytes;
-
-    if (offset + length > end) throw new RangeError("not enough data for decode");
+    checkBufferLengthForDecode(this.codecName, offset, end, length);
 
     this.decodeBytes = offset + length - start;
     return Buffer.from(buffer.slice(offset, offset + length));
