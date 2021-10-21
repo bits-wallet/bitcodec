e070ed5d4b565602958ba73773481bef2645a1ce
Penguen Umut <uuisik@gmail.com>
2021-10-21 16:17:15 +0300

    dist

--- dist/lib/CVarArray.d.ts
+++ dist/lib/CVarArray.d.ts
@@ -2,13 +2,13 @@ import { IBitcodec } from "../models/IBitcodec";
 export declare class CVarArray implements IBitcodec<any[]> {
     private codecName;
     private length;
-    private lengthType;
-    private anyCodec;
+    private lengthCodec;
+    private itemCodec;
     private calcLength;
     encodingLength: (array?: any[] | undefined) => number;
     encodeBytes: number;
     decodeBytes: number;
-    constructor(lengthType: IBitcodec<any>, anyCodec: IBitcodec<any>);
+    constructor(lengthCodec: IBitcodec<any>, itemCodec: IBitcodec<any>);
     encode: (value: any[], buffer?: Buffer | undefined, offset?: number) => Buffer;
     decode: (buffer: Buffer, offset?: number, end?: number | undefined) => any[];
 }
--- dist/util.d.ts
+++ dist/util.d.ts
@@ -1,4 +1,3 @@
 export declare const calcAllLength: <T>(items: T[], calcLength: (item?: T | undefined, itemIndex?: number | undefined, itemOffset?: number | undefined) => number, allOffset?: number) => number;
-export declare function buffer2hex(obj: any): any;
-export declare function isHex(s: string): boolean;
 export declare function hex2buffer(obj: any): any;
+export declare function buffer2hex(obj: any): any;
