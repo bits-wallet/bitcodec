12722a7a1e9c46912c7b57a33daeb937b5a1b16d
Penguen Umut <uuisik@gmail.com>
2021-10-21 16:15:57 +0300

    buffer2hex, hex2buffer fixed mutation

--- src/util.ts
+++ src/util.ts
@@ -8,30 +8,56 @@ export const calcAllLength = <T>(items: T[], calcLength: (item?: T, itemIndex?: 
   }, acc); */
 };
 
-export function buffer2hex(obj: any) {
-  if (Buffer.isBuffer(obj)) return obj.toString("hex");
-  else if (Array.isArray(obj)) obj = obj.map(buffer2hex);
-  else if (typeof obj === "object") {
-    for (var k in obj) {
-      obj[k] = buffer2hex(obj[k]);
+const copy = (obj: any): any => {
+  if (Buffer.isBuffer(obj)) return obj.slice();
+  if (Array.isArray(obj)) return obj.map(copy);
+  if (typeof obj === "object") {
+    const newObj: any = {};
+    for (let k in obj) {
+      newObj[k] = obj.map(copy);
     }
+    return newObj;
+  }
+
+  if (typeof obj === "function") throw "unsupported copy!";
+  return obj;
+};
+
+function isHex(s: string) {
+  return s.length % 2 === 0 && /^[0-9a-f]*$/.test(s.toLowerCase());
+}
+
+export function hex2buffer(obj: any) {
+  if (Buffer.isBuffer(obj)) return copy(obj);
+  else if (typeof obj === "string" && isHex(obj)) return Buffer.from(obj, "hex");
+  else if (typeof obj === "object") {
+    const newObj = copy(obj);
+    for (var k in newObj) {
+      newObj[k] = hex2buffer(newObj[k]);
+    }
+    return newObj;
+  } else if (Array.isArray(obj)) {
+    return copy(obj).map(hex2buffer);
   }
 
   return obj;
 }
 
-export function isHex(s: string) {
-  return s.length % 2 === 0 && /^[0-9a-f]*$/.test(s.toLowerCase());
-}
-
-export function hex2buffer(obj: any) {
-  if (Buffer.isBuffer(obj)) return obj;
-  else if (typeof obj === "string" && isHex(obj)) return Buffer.from(obj, "hex");
-  else if (typeof obj === "object") {
-    for (var k in obj) {
-      obj[k] = hex2buffer(obj[k]);
+export function buffer2hex(obj: any) {
+  if (Buffer.isBuffer(obj)) {
+    const newBuffer = copy(obj);
+    return newBuffer.toString("hex");
+  } else if (Array.isArray(obj)) {
+    let newObj = copy(obj);
+    newObj = newObj.map(buffer2hex);
+    return newObj;
+  } else if (typeof obj === "object") {
+    const newObj = copy(obj);
+    for (var k in newObj) {
+      newObj[k] = buffer2hex(newObj[k]);
     }
-  } else if (Array.isArray(obj)) obj = obj.map(hex2buffer);
+    return newObj;
+  }
 
   return obj;
 }
