2a46dbc1a7ff04febbe65894cb4de8cf54c06823
Penguen Umut <uuisik@gmail.com>
2021-10-21 18:20:58 +0300

    number tests

---
+++ tests/number.test.ts
@@ -0,0 +1,34 @@
+import { buffer2hex, hex2buffer } from "../src";
+import datas from "./data/number";
+
+datas.forEach((data, i) => {
+  test("array encodeBytes i:" + i, () => {
+    expect(data.codec.encodeBytes).toEqual(data.encodeBytes);
+  });
+});
+
+datas.forEach((data, i) => {
+  test("array decodeBytes i:" + i, () => {
+    expect(data.codec.decodeBytes).toEqual(data.decodeBytes);
+  });
+});
+
+datas.forEach((data, i) => {
+  test("array encodingLength i:" + i, () => {
+    expect(data.codec.encodingLength(data.obj)).toEqual(data.encodingLength);
+  });
+});
+
+datas.forEach((data, i) => {
+  test("array decode i:" + i, () => {
+    const obj = buffer2hex(data.codec.decode(hex2buffer(data.hex)));
+    expect(obj).toEqual(data.obj);
+  });
+});
+
+datas.forEach((data, i) => {
+  test("array encode i:" + i, () => {
+    const hex = buffer2hex(data.codec.encode(hex2buffer(data.obj)));
+    expect(hex).toEqual(data.hex);
+  });
+});
